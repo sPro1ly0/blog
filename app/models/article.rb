99cc15d3699ecf3538f0d55575e8e76c60745e45
Sabrina Becker <sbecka8@gmail.com>
2020-06-07 17:17:34 -0400

    Able to delete comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,8 +2,9 @@ class Article < ApplicationRecord
     # Associating Models
     # Each comment belongs to one article.
     # One article can have many comments.
-    has_many :comments
-    
+    # dependent will delete associated comments if an articles is deleted
+    has_many :comments, dependent: :destroy
+
     # title with min. character length of 5
     validates :title, presence: true, length: { minimum:5 }
 end
