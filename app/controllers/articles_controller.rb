dae0e85bc677c7fdf1a5c27f3ae3bd0141153d99
Sabrina Becker <sbecka8@gmail.com>
2020-06-07 14:08:59 -0400

    Able to show all articles and links to create a new article andindividual articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all
+    end
+
     def show
         # instance variable hold a reference to the article object
         @article = Article.find(params[:id])
